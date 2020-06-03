30acb793b1f77f0037235f632c9f89ae0597cb0d
Baffour Adu Boampong <baffouraduboampong@gmail.com>
2020-06-03 15:45:10 +0000

    Add functionality to write parsed data to date stamped file

--- getHomeBrewJson.rb
+++ getHomeBrewJson.rb
@@ -4,20 +4,34 @@ require 'json'
 
 url = 'https://formulae.brew.sh/api/formula.json'
 directory = "data/homebrew"
+parsed_homebrew_packages = []
 
 puts "- Calling API #{url}"
 uri = URI(url)
 response = Net::HTTP.get(uri)
 
 puts "- Parsing response"
-data = JSON.parse(response)
+packages = JSON.parse(response)
+
+# Create directory if does not exist
+FileUtils.mkdir_p directory unless Dir.exists?(directory)
 
 puts "- Generating datestamp"
 time = Time.new
 date = time.strftime("%Y-%m-%d")
 
-# Create directory if does not exist
-FileUtils.mkdir_p directory unless Dir.exists?(directory)
+packages.each do |package|
+  parsed_homebrew_package = {
+    name: package["name"],
+    fullname: package["fullname"],
+    oldname: package["oldname"],
+    versions: package["versions"]
+  }
+  parsed_homebrew_packages.push(parsed_homebrew_package)
+end
+
 # Writing parsed data to file
-File.write("#{directory}/#{date}.txt", data)
+puts "- Writing data to file"
+File.write("#{directory}/#{date}.txt", parsed_homebrew_packages.join("\n"))
+
 
