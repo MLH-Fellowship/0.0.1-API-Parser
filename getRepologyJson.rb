197a6910a66c1027939296fb3490fcd3ec90311c
Larson Carter <larsoncarter@blazermail.net>
2020-06-02 19:43:30 -0500

    Finished Get Repology Json
    If you are to run getRepologyJson.rb it will download the API response into a .json file nested inside repology-JSON/unformatted

--- getRepologyJson.rb
+++ getRepologyJson.rb
@@ -1,1 +1,9 @@
+require 'net/http'
+require 'json'
 
+url = 'https://repology.org/api/v1/projects/?inrepo=homebrew&outdated=1'
+uri = URI(url)
+response = Net::HTTP.get(uri)
+parsed = JSON.parse(response)
+
+File.write("repology-JSON/unformatted/latest.json", parsed)
