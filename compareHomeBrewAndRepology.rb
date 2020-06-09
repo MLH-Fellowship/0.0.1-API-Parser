dcdce859637d9bc2a6d323b7a05026d87eaca9ea
Elizabeth Tackett <emctackett@gmail.com>
2020-06-09 13:12:19 -0500

    store outdated packages in list of objects.

--- compareHomeBrewAndRepology.rb
+++ compareHomeBrewAndRepology.rb
@@ -10,27 +10,26 @@ end
 repology_file = get_latest_file("data/repology")
 homebrew_file = get_latest_file("data/homebrew")
 directory = "data/outdatedpacakges"
-outdated_package = []
+outdated_package_list = []
 
 puts "- Comparing Repology file: #{repology_file} to #{homebrew_file}"
-File.foreach(repology_file) { |line|
-  line_hash = eval(line) 
+File.foreach(repology_file) do |line|
+  line_hash = eval(line)
   packagename = line_hash['packagename']
-  newestversion  = line_hash['newestversion']
+  newestversion = line_hash['newestversion']
 
   rx = Regexp.new(Regexp.escape(packagename), true)
 
   IO.foreach(homebrew_file) do |line|
     if line[rx]
-      line_hash = eval(line) 
-      package = line_hash["name"] + "(" + newestversion + ")"
-      outdated_package.push(package)
+      line_hash = eval(line)
+      package = {}
+      package["name"] = line_hash["name"]
+      package["latest_version"] = newestversion
+      outdated_package_list.push(package)
     end
   end
- 
-}
-
-# puts outdated_package_names
+end
 
 # Create directory if does not exist
 FileUtils.mkdir_p directory unless Dir.exists?(directory)
@@ -41,4 +40,4 @@ date_time = Time.new.strftime("%Y-%m-%dT%H_%M_%S")
 
 # Writing parsed data to file
 puts "- Writing data to file"
-File.write("#{directory}/#{date_time}.txt", outdated_package.join("\n"))
+File.write("#{directory}/#{date_time}.txt", outdated_package_list)
