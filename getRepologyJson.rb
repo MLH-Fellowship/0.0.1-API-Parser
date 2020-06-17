1f57bc98ee77fb9adb40c6952668d6105a2989e1
Elizabeth Tackett <emctackett@gmail.com>
2020-06-17 09:41:54 -0500

    parse and filter homebrew pckg data

--- getRepologyJson.rb
+++ getRepologyJson.rb
@@ -13,46 +13,57 @@ def parse_repology(lastpackage='')
   return JSON.parse(response)
 end
 
-directory = "data/repology"
-outdated_repology_packages = []
+def filter_homebrew(json)
+  result = {}
 
-packages = parse_repology()
-last_index = packages.size - 1
-response_size = packages.size
-
-while response_size > 1  do
-  puts "- Paginating"
-  last_package = packages.keys[last_index]
-  response = parse_repology("#{last_package}/")
-  response_size = response.size
-  packages.merge!(response)
-  last_index = packages.size - 1
-end
-
-packages.each do |package|
-  parsed_outdated_package = {}
-  parsed_outdated_package["packagename"] = package[0]
-
-  for project in package[1] do
-    result = package[1].find {|item| item["status"] == "newest" or item["status"] == "devel" }
-    parsed_outdated_package["newestversion"] = result["version"]
-
-    if project["repo"] == "homebrew"
-      parsed_outdated_package["srcname"] = project["srcname"]
-      parsed_outdated_package["visiblename"] = project["visiblename"]
-      parsed_outdated_package["currentversion"] = project["version"]
-    end
+  json.each do |pckg, data|
+    homebrew_data = data.select { |repo| repo['repo'] == 'homebrew' }
+    result[pckg] = homebrew_data if !homebrew_data.empty?
   end
-  outdated_repology_packages.push(parsed_outdated_package)
+
+  result
 end
 
-# Create directory if does not exist
-FileUtils.mkdir_p directory unless Dir.exists?(directory)
-
-puts "- Generating datetime stamp"
-#Include time to the filename for uniqueness when fetching multiple times a day
-date_time = Time.new.strftime("%Y-%m-%dT%H_%M_%S")
-
-# Writing parsed data to file
-puts "- Writing data to file"
-File.write("#{directory}/#{date_time}.txt", outdated_repology_packages.join("\n"))
+# directory = "data/repology"
+# outdated_repology_packages = []
+#
+# packages = parse_repology()
+# last_index = packages.size - 1
+# response_size = packages.size
+#
+# while response_size > 1  do
+#   puts "- Paginating"
+#   last_package = packages.keys[last_index]
+#   response = parse_repology("#{last_package}/")
+#   response_size = response.size
+#   packages.merge!(response)
+#   last_index = packages.size - 1
+# end
+#
+# packages.each do |package|
+#   parsed_outdated_package = {}
+#   parsed_outdated_package["packagename"] = package[0]
+#
+#   for project in package[1] do
+#     result = package[1].find {|item| item["status"] == "newest" or item["status"] == "devel" }
+#     parsed_outdated_package["newestversion"] = result["version"]
+#
+#     if project["repo"] == "homebrew"
+#       parsed_outdated_package["srcname"] = project["srcname"]
+#       parsed_outdated_package["visiblename"] = project["visiblename"]
+#       parsed_outdated_package["currentversion"] = project["version"]
+#     end
+#   end
+#   outdated_repology_packages.push(parsed_outdated_package)
+# end
+#
+# # Create directory if does not exist
+# FileUtils.mkdir_p directory unless Dir.exists?(directory)
+#
+# puts "- Generating datetime stamp"
+# #Include time to the filename for uniqueness when fetching multiple times a day
+# date_time = Time.new.strftime("%Y-%m-%dT%H_%M_%S")
+#
+# # Writing parsed data to file
+# puts "- Writing data to file"
+# File.write("#{directory}/#{date_time}.txt", outdated_repology_packages.join("\n"))
