16e2ece578705c80fd4f37d043b79772fffbf137
Elizabeth Tackett <emctackett@gmail.com>
2020-06-24 08:35:10 -0500

    typo and extraneous comment removal

--- printPackageUpdates.rb
+++ printPackageUpdates.rb
@@ -24,18 +24,6 @@ outdated_repology_packages = api_parser.parse_repology_api()
 # hash keys of interest: 'name' and 'versions'=> 'stable'
 brew_formulas = api_parser.parse_homebrew_formulas()
 
-formatted_utdated_packages = api_parser.validate_packages(outdated_repology_packages, brew_formulas)
+formatted_outdated_packages = api_parser.validate_packages(outdated_repology_packages, brew_formulas)
 
-puts formatted_utdated_packages
-# homebrew_formula = HomebrewFormula.new
-# homebrew_formula.format_data(repology_data, brew_data)
-
-# # An array of hashes. Each hash has name, brew_v, and latest_v keys.
-# brew_commands_response = brew_livecheck_and_open_pr_check(repology_data.keys)
-
-
-# # a hash of hashes
-# # hash keys are package names
-# # values- {'brew_version: xx', repology_latest: xx, livecheck_latest: xx or not found}
-# combined_version_data = compare_version_data(repology_data, brew_data, brew_commands_response)
-# display_version_data(combined_version_data)
+puts formatted_outdated_packages
